ec41c9de5608f43dbcfe134623aa7decf91fec6d
OmarAouini <aouini.omar93@gmail.com>
2020-09-07 19:50:16 +0200

    added id notation to User

--- springboot-crud/src/main/java/com/aouin/springbootcrud/model/User.java
+++ springboot-crud/src/main/java/com/aouin/springbootcrud/model/User.java
@@ -1,10 +1,12 @@
 package com.aouin.springbootcrud.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
 
+    @Id
     private Integer id;
     private String email;
     private String username;

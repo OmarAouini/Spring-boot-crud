850de98011e538c7b6534c567711f7174b222355
OmarAouini <aouini.omar93@gmail.com>
2020-09-09 19:24:35 +0200

    cleanup

--- springboot-crud/src/main/java/com/aouin/springbootcrud/service/impl/ArticleServiceImpl.java
+++ springboot-crud/src/main/java/com/aouin/springbootcrud/service/impl/ArticleServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
